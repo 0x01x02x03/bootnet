33fca946ae89417652ea4ae0f6277d7a56c36b83
Bram Mittendorff <b.mittendorff@itmundi.nl>
2015-07-31 14:22:50 +0200

    count.py ready for python3 without shodan api

--- count.py
+++ count.py
@@ -1,17 +1,15 @@
-import sys
-import shodan
+import sys, json, urllib3, certifi
 
 shodan_keyfile = open("shodan.key", "r");
 shodan_key = shodan_keyfile.read().strip();
-shodan_api = shodan.Shodan(shodan_key);
+shodan_url = "https://api.shodan.io/shodan/"
 
 if len(sys.argv)>1:
-	try:
-		results = shodan_api.search(sys.argv[1]);
-		print('Results found: %s' % results['total']);
-
-	except (shodan.APIError, e):
-		print('Error: %s' % e);
-
+	request_url = shodan_url+"host/count?key="+shodan_key+"&query="+sys.argv[1]
+	h = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+	r = h.request("GET", request_url)
+	j = json.loads(r.data.decode())
+	print(j["total"])
+	
 else:
 	print("Usage %s <search query>" % sys.argv[0]);
